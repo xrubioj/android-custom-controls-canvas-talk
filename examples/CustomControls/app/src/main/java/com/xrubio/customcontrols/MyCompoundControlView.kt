37b63468fae931d793091a894f604af5fb05da99
Xavier Rubio Jansana <xrubioj@users.noreply.github.com>
2019-09-26 16:02:10 +0200

    Properties examples for MyCompoundControlView (+ file rename)

--- examples/CustomControls/app/src/main/java/com/xrubio/customcontrols/MyCompoundControlView.kt
+++ examples/CustomControls/app/src/main/java/com/xrubio/customcontrols/MyCompoundControlView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
 
 class MyCompoundControlView @JvmOverloads constructor(
     context: Context?,
@@ -12,8 +13,20 @@ class MyCompoundControlView @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
 
+    private val titleTextView by lazy { findViewById<TextView>(R.id.title) }
+    private val subTitleTextView by lazy { findViewById<TextView>(R.id.subtitle) }
+
+    var title: String
+        get() = titleTextView.text.toString()
+        set(value) { titleTextView.text = value }
+
+    var subTitle: String
+        get() = subTitleTextView.text.toString()
+        set(value) { subTitleTextView.text = value }
+
     init {
         View.inflate(context, R.layout.my_compount_control_layout, this)
         this.orientation = VERTICAL
     }
+
 }
